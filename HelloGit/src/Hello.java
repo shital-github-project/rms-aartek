e71323fd7ec29821fab691f179f2cc80c95a620f
shital <shital@mrcool>
2015-06-30 14:50:44 -0700

    sandeep

--- HelloGit/src/Hello.java
+++ HelloGit/src/Hello.java
@@ -4,7 +4,7 @@ public class Hello {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("thuis is shital kumar patel");
-		siuidsffou
+		System.out.println("thuis is shital kumar patel");
 
 	}
 
